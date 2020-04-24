f30685e52ce35336f8f56de7992b58b6137abc4f
rtauler <rtauler@gmail.com>
2020-04-24 16:24:14 +0200

    first result added

---
+++ src/components/Result/Result.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './Result.module.scss';
+
+const Result: React.FC = () => (
+<div className={styles.Result}>
+  <div className={styles.result_card}>
+    <div className={styles.position_indicator}>
+      <p>1</p>
+    </div>
+    <div className={styles.company_logo}>
+      <img src={require('../../assets/logos/si.svg')} />
+    </div>
+    <div className={styles.review_link}>
+      <a href="#">Review</a>
+    </div>
+    <div className={styles.info}>
+      <p className={styles.type_indicator}>Bonus</p>
+      <p className={styles.amount}>$200</p>
+    </div>
+    <div className={styles.expand}></div>
+  </div>
+  </div>
+);
+
+export default Result;
