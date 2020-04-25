464d65eeac4546e132620167ec76ec63bdd031c7
rtauler <rtauler@gmail.com>
2020-04-25 12:46:09 +0200

    added comments on list

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import styles from './List.module.scss';
 import Result from '../Result/Result';
 
+//load external json with item list
 import Pages from './pages.json';
 
 class List extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
+    //define initial state of sort order
     this.state = { isToggleOn: false };
     this.handleClick = this.handleClick.bind(this);
   }
 
+  //define handleclick and prevent update on click on sort anchor
   handleClick(e: { preventDefault: () => void; }) {
     e.preventDefault();
     this.setState((prevState: { isToggleOn: any; }) => ({
@@ -19,18 +22,22 @@ class List extends React.Component<any, any> {
   }
 
   render() {
+    //establish logic on what to do when clicking on the sort anchor
     if (this.state.isToggleOn === true) {
       Pages.sort(function(a, b) { 
+        //sort alfabeticaly
         return a.name.localeCompare(b.name);
       });
     } else {
       Pages.sort(function(a, b) { 
+        //sort by id
         return a.id - b.id;
       });
     }
 
     return (
       <div className={styles.List}>
+        {/* click on sort anchor calls handle click function to sort the list items */}
         <a href="test.html" onClick={this.handleClick}>sort alfabeticaly</a>
         {
           Pages.map(function (item, index) {
