efdbf738fe5c4c939593afcab87126f221b54b6e
rtauler <rtauler@gmail.com>
2020-04-25 12:30:47 +0200

    working state sort without page update

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -7,59 +7,31 @@ import Pages from './pages.json';
 class List extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
-    this.state = { isToggleOn: true };
+    this.state = { isToggleOn: false };
     this.handleClick = this.handleClick.bind(this);
   }
+
   handleClick(e: { preventDefault: () => void; }) {
     e.preventDefault();
     this.setState((prevState: { isToggleOn: any; }) => ({
       isToggleOn: !prevState.isToggleOn
     }));
   }
+
   render() {
-
-    // let sort_alf = true;
-
-    // if (sort_alf === true) {
-    //   Pages.sort(function(a, b) { 
-    //     return a.name.localeCompare(b.name);
-    //   });
-    // } else {
-    //   Pages.sort(function(a, b) { 
-    //     return a.id - b.id;
-    //   });
-    // }
-
-
-    // function handleClick(e: { preventDefault: () => void; }) {
-    //   e.preventDefault();
-    //   console.log('The link was clicked.');
-    //   if (sort_alf === false) {
-    //     sort_alf = true;
-    //     console.log('sort_alf changed from false to true');
-    //     Pages.sort(function (a, b) {
-    //       return a.name.localeCompare(b.name);
-    //     });
-    //   } else if (sort_alf === true) {
-    //     sort_alf = false;
-    //     console.log('sort_alf changed from true to false');
-    //     Pages.sort(function (a, b) {
-    //       return a.id - b.id;
-    //     });
-    //   }
-    // }
+    if (this.state.isToggleOn === true) {
+      Pages.sort(function(a, b) { 
+        return a.name.localeCompare(b.name);
+      });
+    } else {
+      Pages.sort(function(a, b) { 
+        return a.id - b.id;
+      });
+    }
 
     return (
-
-
       <div className={styles.List}>
-
-        <button onClick={this.handleClick}>
-          {this.state.isToggleOn ? 'ON' : 'OFF'}
-        </button>
-
         <a href="test.html" onClick={this.handleClick}>sort alfabeticaly</a>
-        <p>hello</p>
         {
           Pages.map(function (item, index) {
             return (
@@ -73,7 +45,6 @@ class List extends React.Component<any, any> {
             )
           })
         }
-
       </div>
     );
   }
