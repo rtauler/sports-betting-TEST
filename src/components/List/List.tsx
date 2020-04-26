5d96443d17470a2577e3347caf1ffb01a3daac34
rtauler <rtauler@gmail.com>
2020-04-26 17:18:09 +0200

    responsiveness logic for card/row logic

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -10,7 +10,7 @@ class List extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
     //define initial state of sort order also define if item is card
-    this.state = { isSortAlf: false, isCard: false, Pages: Pages };
+    this.state = { isSortAlf: false, isCard: true, Pages: Pages };
     this.sortItems = this.sortItems.bind(this);
     this.transItems = this.transItems.bind(this);
   }
@@ -26,6 +26,9 @@ class List extends React.Component<any, any> {
   //define transCard and prevent update on click on card anchor
   transItems(e: { preventDefault: () => void; }) {
     e.preventDefault();
+    if (window.innerWidth < 520) {
+      this.setState({isCard: false});
+    }
     this.setState((prevState: { isCard: any; }) => ({
       isCard: !prevState.isCard
     }));
@@ -45,21 +48,13 @@ class List extends React.Component<any, any> {
         return a.id - b.id;
       });
     }
-    //establish logic on what to do when click on chane view anchor
-    if (this.state.isCard === true) {
-
-    }
-    else {
-
-    }
 
     return (
       <div className={styles.List}>
         {/* click on sort anchor calls handle click function to sort the list items */}
         <div className={styles.filter_area}>
           <a href="test.html" className={styles.sort_filter_link} onClick={this.sortItems}>Sort Alfabeticaly</a>
-          <p> </p>
-          <a href="test.html" className={styles.sort_filter_link} onClick={this.transItems}>Change View</a>
+          <a href="test.html" className={styles.sort_filter_card} onClick={this.transItems}>Change View</a>
         </div>
 
         {/* check if component is card or not */}
@@ -77,9 +72,8 @@ class List extends React.Component<any, any> {
               amount: React.ReactNode;
               page: React.ReactNode;
               seo: React.ReactNode;
-              isCard : React.ReactNode;
+              isCard: React.ReactNode;
             }, i: any) => {
-              const isCardStatus = this.state.isCard;
               return (
                 <Result
                   id={item.id}
@@ -92,7 +86,7 @@ class List extends React.Component<any, any> {
                   amount={item.amount}
                   page={item.page}
                   seo={item.seo}
-                  isCard={isCardStatus}
+                  isCard={this.state.isCard}
                 />
               )
             })
